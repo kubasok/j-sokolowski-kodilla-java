3c3c2afbe69c2f40fecc15886fdb21c7e953e2ac
kubasok <jakubsokolowski@hotmail.com>
2017-08-05 21:44:07 +0200

    Calc commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator newCalculator = new Calculator();
+
+        int resultAdd = newCalculator.add(5,1);
+        int resultSubtract = newCalculator.subtract(5,1);
+
+        if (resultAdd == 6){
+            System.out.println("Test Add: OK");
+        } else {
+            System.out.println("Test Add: Error");
+        }
+
+        if (resultSubtract == 4){
+            System.out.println("Test Subtract: OK");
+        } else {
+            System.out.println("Test Subtract: Error");
+        }
     }
 }
