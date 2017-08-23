60478f66b5e71e30182b011a361b04ac51565afd
kubasok <jakubsokolowski@hotmail.com>
2017-08-23 23:05:08 +0200

    2 commit flights

--- kodilla-exception/src/main/java/com/kodilla/exception/exceptionTest/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/exceptionTest/FlightFinder.java
@@ -4,24 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FlightFinder {
-    public void findFlight(Flight flight) throws RouteNotFoundException {
-        Map<String, Boolean> airportMap = new HashMap<>();
+    Map<String, Boolean> airportMap = new HashMap<>();
+    public FlightFinder(){
         airportMap.put("Warsaw Modlin",true);
         airportMap.put("Warsaw Chopin",false);
         airportMap.put("London Heathrow",true);
         airportMap.put("Madrid Barajas",false);
         airportMap.put("Buenos Aires Ezeiza",false);
         airportMap.put("Cracow Balice",true);
+    }
+    public Boolean findFlight(Flight flight) throws RouteNotFoundException {
 
-        try {
-            if (airportMap.get(flight.getDepartureAirport()).booleanValue() && airportMap.get(flight.getArrivalAirport()).booleanValue()) {
-                System.out.println("Flight found:\nDeparture: " + flight.getDepartureAirport() +
-                "\nArrival: " + flight.getArrivalAirport());
-            } else {
-                System.out.println("There is no such flight.");
-            }
-        } catch (NullPointerException e) {
+        if (airportMap.get(flight.getArrivalAirport()) == null) {
             throw new RouteNotFoundException();
+        } else {
+            return airportMap.get(flight.getArrivalAirport());
         }
+
     }
 }
---
+++ kodilla-exception/src/test/java/com/kodilla/exception/exceptionTest/exceptionTestSuite.java
@@ -0,0 +1,66 @@
+package com.kodilla.exception.exceptionTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class exceptionTestSuite {
+    @Test
+    public void testFindFlightFalse() {
+        FlightFinder finder = new FlightFinder();
+        Flight flightSearched = new Flight("Warsaw Modlin", "Buenos Aires Ezeiza");
+        Boolean result = null;
+        try {
+            result = finder.findFlight(flightSearched);
+            if (result) {
+                System.out.println("Flight found!");
+            } else {
+                System.out.println("Flight not found.");
+            }
+        } catch (RouteNotFoundException e) {
+            System.out.println("Route not found. Check the airports names.");
+        } finally {
+            System.out.println("Search has ended!");
+        }
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void testFindFlightTrue() {
+        FlightFinder finder = new FlightFinder();
+        Flight flightSearched = new Flight("Warsaw Modlin", "Cracow Balice");
+        Boolean result = null;
+        try {
+            result = finder.findFlight(flightSearched);
+            if (result) {
+                System.out.println("Flight found!");
+            } else {
+                System.out.println("Flight not found.");
+            }
+        } catch (RouteNotFoundException e) {
+            System.out.println("Route not found. Check the airports names.");
+        } finally {
+            System.out.println("Search has ended!");
+        }
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void testFindFlightNull() {
+        FlightFinder finder = new FlightFinder();
+        Flight flightSearched = new Flight("Warsaw Modlin", "Test Airport");
+        Boolean result = null;
+        try {
+            result = finder.findFlight(flightSearched);
+            if (result) {
+                System.out.println("Flight found!");
+            } else {
+                System.out.println("Flight not found.");
+            }
+        } catch (RouteNotFoundException e) {
+            System.out.println("Route not found. Check the airports names.");
+        } finally {
+            System.out.println("Search has ended!");
+        }
+        Assert.assertNull(result);
+    }
+}
