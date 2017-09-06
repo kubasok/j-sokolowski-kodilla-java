b67404016cdb7e37f85f18bdde37bb1811d0e69b
kubasok <jakubsokolowski@hotmail.com>
2017-09-06 19:44:33 +0200

    2 logger

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,6 +1,7 @@
 package com.kodilla.patterns.singleton;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class LoggerTestSuite {
@@ -9,6 +10,14 @@ public class LoggerTestSuite {
     public void testLogger() {
         Logger.getInstance().log("Doing sth");
         String lastLog = Logger.getInstance().getLastLog();
+        Assert.assertEquals("Doing sth", lastLog);
+    }
+
+    @Test
+    public void testLogger2() {
+        Logger testLogger = Logger.getInstance();
+        testLogger.log("Doing sth");
+        String lastLog = testLogger.getLastLog();
         Assert.assertEquals("Doing sth", lastLog);
     }
 }
