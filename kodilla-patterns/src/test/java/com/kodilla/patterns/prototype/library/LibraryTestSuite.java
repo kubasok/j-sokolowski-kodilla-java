c5b991746af59cc47328acb5982de1f181dab567
kubasok <jakubsokolowski@hotmail.com>
2017-09-09 18:12:03 +0200

    1 library

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -0,0 +1,40 @@
+package com.kodilla.patterns.prototype.library;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class LibraryTestSuite {
+    @Test
+    public void testGetBooks(){
+        Library theLibrary = new Library("Fantasy library");
+        theLibrary.books.add(new Book("Harry Potter", "J.K. Rowling", LocalDate.of(1997,6,26)));
+        theLibrary.books.add(new Book("Lord of the Rings", "J.R.R. Tolkien", LocalDate.of(1954,7,29)));
+        theLibrary.books.add(new Book("The Colour of Magic", "Terry Pratchett", LocalDate.of(1983,11,24)));
+
+        //shallow clone of object board
+        Library clonedLibrary = null;
+        try {
+            clonedLibrary = theLibrary.shallowCopy();
+            clonedLibrary.setName("Fantasy library 2");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+        System.out.println(clonedLibrary.books);
+
+        //deep clone of object board
+        Library deepClonedLibrary = null;
+        try {
+            deepClonedLibrary = theLibrary.deepCopy();
+            deepClonedLibrary.setName("Fantasy library 3");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+        System.out.println(deepClonedLibrary.books);
+
+        Assert.assertEquals(3,theLibrary.getBooks().size());
+        Assert.assertEquals(theLibrary.getBooks(), clonedLibrary.getBooks());
+        Assert.assertNotEquals(theLibrary.getBooks(), deepClonedLibrary.getBooks());
+    }
+}
