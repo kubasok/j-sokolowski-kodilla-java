1af46e7cb64b247a02bce3aa137f3e7230ec226e
kubasok <jakubsokolowski@hotmail.com>
2017-10-06 19:04:23 +0200

    1 hib

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,112 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+
+    @Test
+    public void test12() {
+        Assert.assertEquals(0,0);
+    }
+//    @Autowired
+//    private InvoiceDao invoiceDao;
+////    @Autowired
+////    private ProductDao productDao;
+//
+//    @Test
+//    public void testInvoiceDaoSave() {
+//        //Given
+//        Invoice invoice = new Invoice("1");
+//        Product product = new Product("Test Product");
+//        Product product2 = new Product("Test Product2");
+////        Item item = new Item(new BigDecimal(1), 1, new BigDecimal(1));
+////        Item item2 = new Item(new BigDecimal(1), 2, new BigDecimal(2));
+////        Item item3 = new Item(new BigDecimal(1), 3, new BigDecimal(3));
+////
+////
+////        invoice.getItems().add(item);
+////        invoice.getItems().add(item2);
+////        invoice.getItems().add(item3);
+////
+////        product.getItems().add(item);
+////        product.getItems().add(item3);
+////        product2.getItems().add(item2);
+////
+////
+////        item.setProduct(product);
+////        item.setInvoice(invoice);
+////        item2.setProduct(product2);
+////        item2.setInvoice(invoice);
+////        item3.setProduct(product);
+////        item3.setInvoice(invoice);
+////
+////
+////        //When
+////        invoiceDao.save(invoice);
+////
+////        productDao.save(product);
+////        productDao.save(product2);
+////        int id = invoice.getId();
+////
+////        //Then
+////        Assert.assertNotEquals(0, id);
+////
+////        //CleanUp
+////        //taskListDao.delete(id);
+//    }
+//
+//    @Test
+//    public void testInvoiceDaoSave1() {
+//        //Given
+//        Invoice invoice = new Invoice("1");
+//        Product product = new Product("Test Product");
+//        Product product2 = new Product("Test Product2");
+//        Item item = new Item(product, new BigDecimal(1), 1, new BigDecimal(1));
+//        Item item2 = new Item(product2, new BigDecimal(1), 2, new BigDecimal(2));
+//        Item item3 = new Item(product, new BigDecimal(1), 3, new BigDecimal(3));
+//
+//        invoice.getItems().add(item);
+//        invoice.getItems().add(item2);
+//        invoice.getItems().add(item3);
+//
+//        item.setInvoice(invoice);
+//        item2.setInvoice(invoice);
+//        item3.setInvoice(invoice);
+//
+//
+//        //When
+//        invoiceDao.save(invoice);
+//
+////        productDao.save(product);
+////        productDao.save(product2);
+//        int id = invoice.getId();
+//
+//        //Then
+//        Assert.assertNotEquals(0, id);
+//
+//        //CleanUp
+//        //taskListDao.delete(id);
+//    }
+//
+//    @Autowired
+//    ProductDao productDao;
+//
+//    @Test
+//    public void test1(){
+//        Product product = new Product("11");
+//        Assert.assertNotNull(product);
+//    }
+
+}
