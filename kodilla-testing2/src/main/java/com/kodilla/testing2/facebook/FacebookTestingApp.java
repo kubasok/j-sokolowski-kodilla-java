7aaa329d9505b7bbfdf3ed39f2a0e753a1e49b99
kubasok <jakubsokolowski@hotmail.com>
2017-12-03 22:58:27 +0100

    facebook acc 2

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class FacebookTestingApp {
-//    public static final String XPATH_INPUT = "//html/body/main/section/form/fieldset/input";
-//    public static final String XPATH_TEXTAREA = "//html/body/main/section/form/fieldset[2]/textarea";
     public static final String XPATH_WAIT_FOR = "//select[1]";
     public static final String XPATH_SELECT_DAY = "//span[contains(@class, \"_5k_4\")]/select[1]";
     public static final String XPATH_SELECT_MONTH = "//span[contains(@class, \"_5k_4\")]/select[2]";
@@ -17,12 +15,6 @@ public class FacebookTestingApp {
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.facebook.com/");
-
-//        WebElement searchField = driver.findElement(By.xpath(XPATH_INPUT));
-//        searchField.sendKeys("New robotic task");
-//
-//        WebElement textareaField = driver.findElement(By.xpath(XPATH_TEXTAREA));
-//        textareaField.sendKeys("New robotic content");
 
         while (!driver.findElement(By.xpath(XPATH_WAIT_FOR)).isDisplayed());
 
@@ -38,6 +30,5 @@ public class FacebookTestingApp {
         Select selectYear = new Select(selectComboYear);
         selectDay.selectByValue("1992");
 
-        //searchField.submit();
     }
 }
